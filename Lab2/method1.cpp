55cd4dae5ec500126a9afaac5ad69836487717c4
LikimiaD <somethingforsoftware@gmail.com>
2024-03-20 20:05:01 +0300

    <FIX> Mar-20 Fix and add counters

--- Lab2/method1.cpp
+++ Lab2/method1.cpp
@@ -1,26 +1,30 @@
 #include <cmath>
 #include <iostream>
 
-double fx(const double x) { return (x - 1) * exp(-pow(x, 2) / 2.0); }
-double dfx(const double x) {
+double fx(const double& x) {
+  return (x - 1) * exp(-pow(x, 2) / 2.0);
+}
+
+double dfx(const double& x) {
   return exp(-pow(x, 2) / 2.0) - x * (x - 1) * exp(-pow(x, 2) / 2.0);
 }
-void print(const double& xStar, const double& fStar, const double& k) {
-  std::cout << "Root found at: " << xStar << " with function value: " << fStar
-            << " after " << k << " iterations." << std::endl;
-}
+
 void solve(double a0, double b0, const double epsilon) {
-  double k = 0;
+  int k = 0;
+  int fx_counter = 0;
+  int dfx_counter = 0;
   double xStar = 0;
   double fStar = 0;
 
   while (true) {
     double xMiddle = (a0 + b0) / 2;
     double fPrimeX = dfx(xMiddle);
+    dfx_counter++;
 
     if (std::abs(fPrimeX) <= epsilon) {
       xStar = xMiddle;
       fStar = fx(xStar);
+      fx_counter++;
       break;
     } else {
       k++;
@@ -31,7 +35,11 @@ void solve(double a0, double b0, const double epsilon) {
       }
     }
   }
-  print(xStar, fStar, k);
+
+  std::cout << "Root found at: " << xStar << " with function value: " << fStar
+            << " after " << k << " iterations." << std::endl;
+  std::cout << "Function was called " << fx_counter << " times." << std::endl;
+  std::cout << "Derivative was called " << dfx_counter << " times." << std::endl;
 }
 
 int main() {
--- Lab2/method2.cpp
+++ Lab2/method2.cpp
@@ -1,19 +1,41 @@
 #include <cmath>
 #include <iostream>
 
-double fx(const double x) { return (x - 1) * exp(-pow(x, 2) / 2.0); }
-double dfx(const double x) {
+double fx(const double& x, const bool& returnCounter = false) {
+  static int counter = 0;
+  counter++;
+  if (returnCounter) {
+    return counter;
+  }
+  return (x - 1) * exp(-pow(x, 2) / 2.0);
+}
+double dfx(const double& x, const bool& returnCounter = false) {
+  static int counter = 0;
+  counter++;
+  if (returnCounter) {
+    return counter;
+  }
   return exp(-pow(x, 2) / 2.0) - x * (x - 1) * exp(-pow(x, 2) / 2.0);
 }
-double ddfx(const double x) { return (x * x - 3 * x + 1) * exp(-x * x / 2.0); }
+double ddfx(const double x, const bool& returnCounter = false) {
+  static int counter = 0;
+  counter++;
+  if (returnCounter) {
+    return counter;
+  }
+  return (x * x - 3 * x + 1) * exp(-x * x / 2.0);
+}
 void print(const double& xStar, const double& fStar, const double& k) {
   std::cout << "Root found at: " << xStar << " with function value: " << fStar
             << " after " << k << " iterations." << std::endl;
+  std::cout << "Function was called " << fx(0, true) << " times." << std::endl;
+  std::cout << "First derivative was called " << dfx(0, true) << " times." << std::endl;
+  std::cout << "Second derivative was called " << ddfx(0, true) << " times." << std::endl;
 }
 void solve(const double a0, const double b0, const double epsilon) {
   double xk = (a0 + b0) / 2;
-  double xStar;
-  double fStar;
+  double xStar = 0;
+  double fStar = 0;
   int k = 0;
 
   while (true) {
